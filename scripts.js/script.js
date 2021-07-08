70fdf2a8ff9fea3377db48ba1fab3d008153e2f3
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-07-08 22:49:40 +0300

    Корзина закрывается

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -20,6 +20,19 @@ headerCityButton.addEventListener ('click', () => {
 const subheaderCart = document.querySelector('.subheader__cart');
 const cartOverlay = document.querySelector('.cart-overlay');
 
-subheaderCart.addEventListener('click', () => {
+const cartModalOpen = () => {
     cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.classList.contains('cart__btn-close')) {
+        cartModalClose();
+    }
 });
