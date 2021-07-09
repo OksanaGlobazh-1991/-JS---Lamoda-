3db567772ddff08f9067a2c7762f5bab301ff6a6
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-07-09 15:43:04 +0300

    идеальная блокировка скролла

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -17,7 +17,15 @@ headerCityButton.addEventListener ('click', () => {
 
 const disableScroll = () => {
         const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+        document.body.dbScrollY = window.scrollY;
+
         document.body.style.cssText = `
+            position: fixed;
+            top: ${-window.scrollY}px;
+            left: 0;
+            width: 100%;
+            height: 100vh;
             overflow: hidden;
             padding-right: ${widthScroll}px;
         `;
@@ -25,6 +33,9 @@ const disableScroll = () => {
 
 const enableScroll = () => {
         document.body.style.cssText = '';
+        window.scroll({
+            top: document.body.dbScrollY,
+        })
 
 };
 
